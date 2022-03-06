e5f2f27a54d1617a7fe9d2c5292ac3ce3eb67bc9
mobarak <mobarakhossen2012@gmail.com>
2022-03-06 10:11:56 +0600

    initial commit

---
+++ RemainderApp/RemainderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemainderApp
+//
+//  Created by Mobarak on 3/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
